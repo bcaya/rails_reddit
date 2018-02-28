541b60da099812665f7e33355241126efbf32fd2
Bobby Caya <bob.caya1@gmail.com>
2018-02-28 12:05:17 -0700

    Adds topics and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'sub#index'
+
+  resources :subs do 
+    resources :topics 
+  end  #nested topics inside of sub routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
